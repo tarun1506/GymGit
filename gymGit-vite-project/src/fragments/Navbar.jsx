51a70046d001b543ab4c3b6baedc0dc5d94050eb
tarun1506 <tarunmohan15@hotmail.com>
2024-02-20 16:16:25 -0800

    added more workouts

--- gymGit-vite-project/src/fragments/Navbar.jsx
+++ gymGit-vite-project/src/fragments/Navbar.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 class NavBar extends React.Component {
+  handleLogout = () => {
+    // Reset internal storage
+    localStorage.clear();
+  };
+
   render() {
     return (
       <>
         <nav className="navbar bg-body-tertiarynavbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
+            <Link className="navbar-brand" to="/">
               <img
                 src="src/assets/icons8-dumbell-100.png"
                 alt="Bootstrap"
@@ -15,20 +20,29 @@ class NavBar extends React.Component {
                 height={50}
               />
             </Link>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/about">
-                  About
-                </Link>
-              </li>
-            </ul>
-          </div>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link className="nav-link active" aria-current="page" to="/">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about">
+                    About
+                  </Link>
+                </li>
+              </ul>
+              <button
+                className="btn btn-outline-light"
+                onClick={this.handleLogout}
+              >
+                Logout
+              </button>
+            </div>
           </div>
         </nav>
       </>
--- gymGit-vite-project/src/model/workoutData.js
+++ gymGit-vite-project/src/model/workoutData.js
@@ -5,7 +5,11 @@ const workoutData = [
       { workout: "Barbell Rowing", reps: "3 / 12-10" },
       { workout: "Lat PullDown", reps: "3 / 10" },
       { workout: "Seated Rowing", reps: "3 / 10" },
-      // Add more data for Monday
+      { workout: "Deadlift", reps: "3 / 10" },
+      { workout: "Pull Ups", reps: "3 / 10" },
+      { workout: "Barbell Curl", reps: "3 / 10" },
+      { workout: "Dumbbell Curl", reps: "3 / 10" },
+      { workout: "Hammer Curl", reps: "3 / 10" },
     ],
   },
   {
@@ -13,7 +17,12 @@ const workoutData = [
     data: [
       { workout: "Sleeping Bar Press", reps: "3 / 12-10" },
       { workout: "Inclined Dumbbell Press", reps: "3 / 10" },
-      // Add more data for Tuesday
+      { workout: "Dumbbell Press", reps: "3 / 10" },
+      { workout: "Push Ups", reps: "3 / 10" },
+      { workout: "Dips", reps: "3 / 10" },
+      { workout: "Tricep Pushdown", reps: "3 / 10" },
+      { workout: "Tricep Kickback", reps: "3 / 10" },
+      { workout: "Skull Crushers", reps: "3 / 10" },
     ],
   },
   {
@@ -21,10 +30,54 @@ const workoutData = [
     data: [
       { workout: "Barbell Overhead Press", reps: "3 / 12-10" },
       { workout: "Dumbbell Delt Rise", reps: "3 / 10" },
-      // Add more data for Wednesday
+      { workout: "Front Delt Rise", reps: "3 / 10" },
+      { workout: "Lateral Delt Rise", reps: "3 / 10" },
+      { workout: "Shrugs", reps: "3 / 10" },
+      { workout: "Barbell Upright Row", reps: "3 / 10" },
+      { workout: "Dumbbell Upright Row", reps: "3 / 10" },
+      { workout: "Barbell Shrug", reps: "3 / 10" },
     ],
   },
-  // Add more objects for other days
+  {
+    day: "Thursday",
+    data: [
+      { workout: "Squat", reps: "3 / 12-10" },
+      { workout: "Leg Press", reps: "3 / 10" },
+      { workout: "Leg Curl", reps: "3 / 10" },
+      { workout: "Leg Extension", reps: "3 / 10" },
+      { workout: "Calf Raise", reps: "3 / 10" },
+      { workout: "Seated Calf Raise", reps: "3 / 10" },
+      { workout: "Leg Press Calf Raise", reps: "3 / 10" },
+      { workout: "Standing Calf Raise", reps: "3 / 10" },
+    ],
+  },
+  {
+    day: "Friday",
+    data: [
+      { workout: "Barbell Rowing", reps: "3 / 12-10" },
+      { workout: "Lat PullDown", reps: "3 / 10" },
+      { workout: "Seated Rowing", reps: "3 / 10" },
+      { workout: "Deadlift", reps: "3 / 10" },
+      { workout: "Pull Ups", reps: "3 / 10" },
+      { workout: "Barbell Curl", reps: "3 / 10" },
+      { workout: "Dumbbell Curl", reps: "3 / 10" },
+      { workout: "Hammer Curl", reps: "3 / 10" },
+    ]
+  },
+  {
+    day: "Saturday",
+    data: [
+      { workout: "Sleeping Bar Press", reps: "3 / 12-10" },
+      { workout: "Inclined Dumbbell Press", reps: "3 / 10" },
+      { workout: "Dumbbell Press", reps: "3 / 10" },
+      { workout: "Push Ups", reps: "3 / 10" },
+      { workout: "Dips", reps: "3 / 10" },
+      { workout: "Tricep Pushdown", reps: "3 / 10" },
+      { workout: "Tricep Kickback", reps: "3 / 10" },
+      { workout: "Skull Crushers", reps: "3 / 10" },
+    ]
+  }
+
 ];
 
 export default workoutData;
